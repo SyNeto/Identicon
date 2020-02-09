07bd924d773351ce5f2d512d3f2e6a02d7f1c5d8
Ernesto Jimenez V <netorcido@gmail.com>
2020-02-09 13:57:40 -0600

    Initial commit

---
+++ lib/identicon.ex
@@ -0,0 +1,15 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for `Identicon`.
+  """
+
+  def main(input) do
+    input |> hash_input
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input) |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
+  end
+
+end
